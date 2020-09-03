01b1354089e2c4e62254ea4d0885b03534851870
Taiki Yoshino <44nocycle@gmail.com>
2020-09-03 20:30:24 +0900

    Updates

---
+++ precache-manifest.04703fec8661b1649b209c50366d70ab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99eedf499446f754a5cb878bd4301b4",
+    "url": "/yoshino/index.html"
+  },
+  {
+    "revision": "b52a40c4e17b361c2202",
+    "url": "/yoshino/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12043a9607b1e6b1610a",
+    "url": "/yoshino/static/js/2.13b66721.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yoshino/static/js/2.13b66721.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b52a40c4e17b361c2202",
+    "url": "/yoshino/static/js/main.72b9281c.chunk.js"
+  },
+  {
+    "revision": "ac9ff1413e7fc1a7512a",
+    "url": "/yoshino/static/js/runtime-main.91b1bd6f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yoshino/static/media/logo.5d5d9eef.svg"
+  }
+]);
